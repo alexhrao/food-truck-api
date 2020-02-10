932b5e3cd2b19b27876d6367bdb58bada29e39e9
Alexander Rao <alexhrao@gmail.com>
2020-02-10 10:44:09 -0500

    Add correct view and setup files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from google.cloud import storage, firestore
+import base64
 
 db = firestore.Client()
 
@@ -11,14 +12,14 @@ class LiveView(Resource):
     def get(self, view):
         doc_ref = db.collection(u'last-view').document(view)
         last_view = doc_ref.get().to_dict()
-        file_data = storage.Client().bucket(view).blob(last_view['filename']).download_as_string()
+        file_data = str(base64.b64encode(storage.Client().bucket(view).blob(last_view['filename']).download_as_string()), encoding='utf-8')
         return ({
             'data': file_data,
-            'time': last_view['time_updated']
+            'time': last_view['time_updated'].isoformat()
         })
         
 
 api.add_resource(LiveView, '/api/views/<string:view>')
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=False)
