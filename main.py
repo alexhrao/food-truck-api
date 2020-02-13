3c7f258d9f2ec27a50e68c87b0e493ff10c306ce
Alexander Rao <alexhrao@gmail.com>
2020-02-13 17:42:43 -0500

    Add basic API for getting images for labeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_restful import Resource, Api
 from google.cloud import storage, firestore
 from flask_cors import CORS
@@ -31,9 +31,68 @@ class Views(Resource):
         print(docs)
         return docs
 
+# Image Document output looks like this:
+"""
+{
+    'key': 'filename',
+    'bucket': 'bucket-name',
+    'filename': 'filename.png',
+    'valid': bool,
+    'labels': [
+        {
+            'groupName': 'group-name',
+            'groupType': 'single'|'multiple'|'numeric'
+            'values': 'label-name'|['label-names']|number
+        },
+        {
+            'groupName': 'group-name',
+            'groupType': 'single'|'multiple'|'numeric'
+            'values': 'label-name'|['label-names']|number
+        },...
+    ]
+}
+"""
+class ImageDocument(Resource):
+    def get(self, bucket_name='', image_name=''):
+        if bucket_name == '':
+            # No bucket - just get the first one that hasn't been seen!
+            docs = list(db.collection_group('labels').where('seen', '==', False).limit(1).stream())
+            if len(docs) == 0:
+                return {}
+            doc = docs[0]
+            return self.get(doc.get('bucket'), doc.id)
+        doc_ref = db.collection('images').document(bucket_name).collection('labels').document(image_name)
+        doc = doc_ref.get()
+        if not doc.exists:
+            return {}
+        out = {
+            'key': image_name,
+            'bucket': bucket_name,
+            'filename': '{0}.png'.format(image_name),
+            'valid': doc.get('valid'),
+            'seen': doc.get('seen'),
+            'labels': list()
+        }
+        for labelGroup in doc.get('labels'):
+            label = labelGroup.get('group').get()
+            out['labels'].append({
+                'groupName': label.id,
+                'groupType': label.get('groupType'),
+                'values': labelGroup.get('values')
+            })
+        return out
+
+@app.route('/api/snapshots/<string:bucket_name>/<string:image_name>')
+def snapshot(bucket_name, image_name):
+    file_ref = storage.Client().bucket(bucket_name).blob('{0}.png'.format(image_name))
+    file_data = file_ref.download_as_string()
+    resp = Response(file_data)
+    resp.headers['Content-Type'] = file_ref.content_type
+    return resp
 
 api.add_resource(LiveView, '/api/views/<string:view>')
 api.add_resource(Views, '/api/views')
+api.add_resource(ImageDocument, '/api/images/<string:bucket_name>/<string:image_name>', '/api/images/')
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(host='127.0.0.1', port=8080, debug=True)
